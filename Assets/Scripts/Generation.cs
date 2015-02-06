249a07df158b900f6ebce5f78b01673cd3b3e5e6
SamAmco <samuel.amco@btinternet.com>
2015-02-06 14:49:56 +0000

    stochastic universal sampling programmed, still not working

--- Assets/Scripts/Generation.cs
+++ Assets/Scripts/Generation.cs
@@ -11,19 +11,42 @@ public class Generation
 		this.shipArchives = shipArchives.Sort();
 	}
 
-	public List<ShipArchive> SUSSelection(int numberToSelect)
+	public List<ShipArchive> SUS(uint numberToSelect)
 	{
-		if (numberToSelect > shipArchives.Count || numberToSelect < 0)
-			return null;
-
 		double sumOfFitness = 0;
 		foreach (ShipArchive s in shipArchives)
 		{
 			sumOfFitness += s.fitness;
 		}
+		double dist = 1d / (double)numberToSelect;
+		double startingPoint = (double)Random.Range(0f, 1f);
 
+		int numSelected = 0;
+		List<ShipArchive> selectedArchives = new List<ShipArchive>();
+		while (numSelected < numberToSelect)
+		{
+			double point = startingPoint + (dist * numSelected);
 
+			double fitnessCount = 0;
+			foreach (ShipArchive s in shipArchives)
+			{
+				fitnessCount += getProportionateFitness(s.fitness, sumOfFitness);
+				if (point < fitnessCount)
+				{
+					selectedArchives.Add(s);
+					break;
+				}
+			}
+			
+			numSelected++;
+		}
 
+		return selectedArchives;
+	}
+
+	private double getProportionateFitness(double fitness, double sumOfFitness)
+	{
+		return 1d - (fitness / sumOfFitness);
 	}
 
 }
