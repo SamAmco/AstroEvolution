e3311b78f9aa5427704075dc9fc4e4dd4484a7ee
SamAmco <samuel.amco@btinternet.com>
2015-03-18 12:18:07 +0000

    found a version of the config where they dont spin as much. Probably could still be better.

--- Assets/Scripts/Config.cs
+++ Assets/Scripts/Config.cs
@@ -3,16 +3,16 @@ using System.Collections;
 
 public class Config
 {
-	public const float ENGINE_POWER = 60;//50;
-	public const float BLOCK_MASS = 0.4f;//0.3f;
-	public const float BLOCK_DRAG = 0.2f;//0.4f;
-	public const float BLOCK_ANGULAR_DRAG = 0.2f;//0.4f;
+	public const float ENGINE_POWER = 30;//50;
+	public const float BLOCK_MASS = 0.2f;//0.3f;
+	public const float BLOCK_DRAG = 0.3f;//0.4f;
+	public const float BLOCK_ANGULAR_DRAG = 0.5f;//0.4f;
 	public const float CHANCE_OF_CHILD_NODE = 0.3f;
 	public const int MAX_SHIP_DEPTH = 5;
-	public const float MAX_CHILD_ROTATION = 20f;
-	public const int STANDARD_GENERATION_TIME = 60;
-	public const float SIMULATION_TIME_SCALE = 2.0f;
-	public const float FUEL_COST = 100f;
+	public const float MAX_CHILD_ROTATION = 5f;
+	public const int STANDARD_GENERATION_TIME = 50;
+	public const float SIMULATION_TIME_SCALE = 1.0f;
+	public const float FUEL_COST = 0f;
 	public const float MUTATION_PROBABILITY = 0.15f;
 	public const float STILLNESS_COST =	1000000f;
 	public const float BLOAT_MULTIPLIER = 1.01f;
